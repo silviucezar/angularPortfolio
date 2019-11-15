763fc227b34297db7f3f9d2a5cbd04887d8ca64c
unknown <cimpoeru.silviu@gmail.com>
2019-11-15 07:41:56 +0200

    ...

--- dev_Presentation/FE/src/app/app.component.ts
+++ dev_Presentation/FE/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpService } from 'src/Services/http.service';
 import { RenderingService } from "src/Services/rendering.service";
-import { BehaviorSubject, Observable } from 'rxjs';
-import { RenderDetails } from "src/Interfaces/renderDetails";
+import { RenderDetails } from "src/Classes/renderDetails";
 
 @Component({
   selector: 'app-root',
@@ -15,30 +14,22 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild("scrollCanvas", { static: true }) scrollCanvas: ElementRef;
   private scrollCanvasContext: CanvasRenderingContext2D;
 
-  renderDetails: RenderDetails;
+  private renderDetails = new RenderDetails();
 
   constructor
     (
       private http: HttpService,
       private renderingService: RenderingService
     ) {
-    this.renderingService.getRenderDetails().subscribe(renderDetails => {
-      this.renderDetails = renderDetails;
-    });
+
   }
 
   ngOnInit() {
-    console.log(navigator)
-    console.log(window)
-    console.log(this.renderDetails);
+    console.log(this.renderDetails)
   }
 
   ngAfterViewInit() {
     this.scrollCanvasContext = (<HTMLCanvasElement>this.scrollCanvas.nativeElement).getContext("2d");
     const ctx = this.scrollCanvasContext;
-  }
-
-  testvideo(e) {
-    console.log(e);
   }
 }
