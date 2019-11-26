adf57945c58b8c4cf956720eafbe74c9ede1b079
unknown <cimpoeru.silviu@gmail.com>
2019-11-26 22:16:50 +0200

    inserted additional tables creation queries

--- dev_Presentation/BE/src/_Database/db_Creation.ts
+++ dev_Presentation/BE/src/_Database/db_Creation.ts
@@ -24,26 +24,68 @@ export class DBCreation {
         this.PGQuery.QueryArrays = [
             [
                 `CREATE TABLE params (
-                        params TEXT,
+                        params TEXT NOT NULL,
                         id int NOT NULL PRIMARY KEY
                     )`,
-                `INSERT INTO params(params,id) VALUES ('{
-                        "isParamsTableCreated": "1"
-                    }',1)`
+                `INSERT INTO params(params,id) VALUES ('{}',1)`
             ],
             [
                 `CREATE TABLE mainprofiledetails (
-                        firstname VARCHAR(20),
-                        midlename VARCHAR(20),
-                        lastname VARCHAR(50),
-                        email VARCHAR(50),
-                        latestrole VARCHAR(50),
-                        age SMALLINT,
-                        birthdate TEXT,
-                        gender VARCHAR(10),
-                        phoneno SMALLINT,
-                        id INT NOT NULL PRIMARY KEY
-                        )`
+                    image TEXT NOT NULL,
+                    firstname VARCHAR(20) NOT NULL,
+                    lastname VARCHAR(50) NOT NULL,
+                    email VARCHAR(50) NOT NULL,
+                    latestrole VARCHAR(50) NOT NULL,
+                    employer VARCHAR(255) NOT NULL,
+                    age SMALLINT NOT NULL,
+                    birthdate TEXT NOT NULL,
+                    gender VARCHAR(10) NOT NULL,
+                    phoneno VARCHAR(20) NOT NULL,
+                    id INT NOT NULL PRIMARY KEY
+                )`,
+                `INSERT INTO mainprofiledetails VALUES (
+                    'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659652_960_720.png',
+                    'Silviu-Cezar',
+                    'Cimpoeru',
+                    'cimpoeru.silviu@gmail.com',
+                    'Front End Developer',
+                    'REI Development Services',
+                    '27',
+                    '19-Dec-1991',
+                    'M',
+                    '+40766 516 510',
+                    1
+                )`
+            ],
+            [
+                `CREATE TABLE text_translations (
+                    text TEXT NOT NULL,
+                    locale VARCHAR(10) NOT NULL,
+                    prefix TEXT NOT NULL PRIMARY KEY
+                )`,
+                `INSERT INTO text_translations (text,locale,prefix) VALUES
+                    ('image','all','intro_image_all'),
+                    ('Rolul Actual','ro_ro','intro_current_role_ro'),
+                    ('Current Role','en_US','intro_current_role_en'),
+                    ('Nume','ro_ro','intro_last_name_ro'),
+                    ('Last Name','en_US','intro_last_name_en'),
+                    ('Prenume','ro_ro','intro_first_name_ro'),
+                    ('First Name','en_US','intro_first_name_en'),
+                    ('Despre Mine','ro_ro','intro_about_me_short_ro'),
+                    ('About Me','en_US','intro_about_me_short_en'),
+                    ('intro_about_me_detailed_ro','ro_ro','intro_about_me_detailed_ro'),
+                    ('intro_about_me_detailed_en','en_US','intro_about_me_detailed_en'),
+                    ('Vârsta','ro_ro','intro_age_ro'),
+                    ('Age','en_US','intro_age_en'),
+                    ('Data Nașterii','ro_ro','intro_birth_date_ro'),
+                    ('Birth Date','en_US','intro_birth_date_en'),
+                    ('Genul','ro_ro','intro_gender_ro'),
+                    ('Gender','en_US','intro_gender_en'),
+                    ('Email','ro_ro','intro_email_ro'),
+                    ('Email','en_US','intro_email_en'),
+                    ('Telefon','ro_ro','intro_phone_no_ro'),
+                    ('Phone No.','en_US','intro_phone_no_en')
+                `
             ]
         ];
 
@@ -70,10 +112,14 @@ export class DBCreation {
         if (AdditionalFuctionality) _AdditionalFuctionality();
         if (MainQueryIndex === this.PGQuery.QueryArrays.length) {
             console.log(MainQueryIndex === this.PGQuery.QueryArrays.length, MainQueryIndex, this.PGQuery.QueryArrays.length)
-            return Promise.all(this.PromiseArr).then(result => {
-                console.log(result);
-                console.log("done");
-            });
+            return Promise.all(this.PromiseArr)
+                .then(result => {
+                    console.log(result);
+                    console.log("done");
+                })
+                .catch(e => {
+                    console.log(e);
+                })
         } else {
             const TransactionQuery = this.PGQuery.QueryArrays[MainQueryIndex];
             this.PromiseArr.push(
--- dev_Presentation/FE/src/Services/http.service.ts
+++ dev_Presentation/FE/src/Services/http.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class HttpService {
 
+  public readonly doGetRequest:(url:string,body:any) => Promise<any>;
+
   constructor(
     private http: HttpClient
-  ) { }
-
-  getRequest(url: string, body: any): Promise<any> {
-    return new Promise((res, rej) => {
-      this.http.get<any>(`http://localhost:8080/api${url}`, {
-        params: body
-        // responseType : "arraybuffer" as any
-      }).subscribe(value => {
-        res(value);
-      })
-    });
+  ) { 
+    this.doGetRequest = (url: string, body: any) => {
+      return new Promise((res, rej) => {
+        this.http.get<any>(`http://localhost:8080/api${url}`, {
+          params: body
+          // responseType : "arraybuffer" as any
+        }).subscribe(value => {
+          res(value);
+        })
+      });
+    }
   }
+  //postrequest
+
+  //query
 }
